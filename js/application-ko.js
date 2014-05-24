cb7e0d072fb81a8a9225235b0171a536ca9379a8
username <username@username-pc>
2014-05-24 11:14:56 +0300

    initial implementation of knockoutjs version;

---
+++ js/application-ko.js
@@ -0,0 +1,78 @@
+/**
+ * Created by username on 5/24/14.
+ */
+(function () {
+	"use strict";
+
+	var utils = {
+		hex4: function () {
+			return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+		},
+		generateId: function () {
+			return [this.hex4(), this.hex4(), this.hex4()].join("-");
+		},
+		blockEvent: function (event) {
+			if (!event) {
+				event = window.event;
+			}
+
+			if (event.stopPropagation) {
+				event.stopPropagation();
+			} else {
+				event.cancelBubble = true;
+			}
+
+			if (event.preventDefault) {
+				event.preventDefault();
+			} else {
+				event.returnValue = false;
+			}
+		}
+	};
+
+	function Product(title, price) {
+		var self = this;
+
+		self.id = utils.generateId();
+		self.title = title;
+		self.price = price;
+	}
+
+	function ProductsViewModel() {
+		var self = this;
+
+		self.products = ko.observableArray([]);
+		self.id = ko.observable("");
+		self.title = ko.observable("");
+		self.price = ko.observable("");
+
+		self.addProduct = function () {};
+		self.editProduct = function (product) {
+			self.id(product.id);
+			self.title = product.title;
+			self.price = product.price;
+		};
+		self.removeProduct = function (product) {
+			if (window.confirm("Are you sure?")) {
+				self.products.remove(product);
+			}
+		};
+		self.formSubmit = function (formElement) {
+			if (self.id()) {
+				self.id = ko.observable("");
+				self.title = ko.observable("");
+				self.price = ko.observable("");
+			} else {
+				self.products.push(new Product(self.title(), self.price()));
+			}
+
+			self.title("");
+			self.price("");
+
+			utils.blockEvent();
+		};
+	}
+
+	ko.applyBindings(new ProductsViewModel());
+
+}());
